bad655f54ace0ae90c53286a8199fd2fb381fe71
Lasse <doodle.11243@gmail.com>
2021-11-05 23:21:08 +0100

    fixed Error while looking into air

--- main.py
+++ main.py
@@ -12,15 +12,22 @@ IMG_NAME = "IDPhoto.png"
 IMG_PATH = os.path.join(FILEPATH, "img", IMG_NAME)
 debug = True
 
-
-
-mcIDpos = [1500, 283, 420, 17]
-x1 = mcIDpos[0]
-y1 = mcIDpos[1]
-x2 = mcIDpos[0] + mcIDpos[2]
-y2 = mcIDpos[1] + mcIDpos[3]
-
 os.chdir("img")
+
+def resize_img(Filepath, shortside, factor):
+    # Resize Image
+    #open image
+    img = Image.open(Filepath)
+    #set size
+    size = (shortside * factor, shortside)
+    #resize image
+    img = img.resize((size), Image.ANTIALIAS)
+    img = img.resize((size), Image.ANTIALIAS)
+    #save image
+    img.save(IMG_PATH)
+    if debug:
+        print("Image resized")
+    return 1
 
 def toclipboard(text):
     command = 'echo | set /p nul=' + text.strip() + '| clip'
@@ -35,22 +42,28 @@ def calc_xy(x, y, w, h):
 
     return x1, y1, x2, y2
 
-def textdetection(Filepath=IMG_PATH):
+def textdetection(Filepath=""):
         # Textdetetion
-        reader = ocr.Reader(["en"], gpu= True)
-        img = cv2.imread(IMG_PATH)
+        reader = ocr.Reader(["en"])
+        img = cv2.imread(Filepath)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         res = reader.readtext(gray)
+        print(res)
         
         #Strip the Output for further processing
-        Output = res[0][1]
-        Output = Output[9:]
+        if not res:
+            Output = -1
+        else:
+            Output = res[0][1]
+            Output = Output[9:]
+            
+        #debug
         if debug:
             print(Output)
 
         return Output
 
-def takescreenshot(Position=[x1, y1, x2, y2]):
+def takescreenshot(Position=("", "", "", "")):
         # Take Screenshot
         pg.screenshot(IMG_NAME)
         if debug:
@@ -59,15 +72,15 @@ def takescreenshot(Position=[x1, y1, x2, y2]):
         
         #crop image to perfect size
         im = Image.open(IMG_PATH)
-        im = im.crop((x1, y1, x2, y2))
+        im = im.crop(Position)
         if debug:
             print("Image cropped")
         im.save(IMG_NAME)
         if debug:
             print("Image saved")
 
-def searchcsv(search_term):
-    with open("IDs.csv", 'r') as csv_file:
+def searchcsv(search_term, Filepath=""):
+    with open(Filepath, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
 
         if debug:
@@ -79,6 +92,11 @@ def searchcsv(search_term):
                     print("Found in CSV")
                     print(row)
                 return row[3]
+        
+        if search_term == -1:
+            if debug:
+                print("Not found in CSV")
+            return -1
                     
             
 if debug:
@@ -89,20 +107,30 @@ while keyboard.is_pressed('Alt+y') == False:
     #take Screenshot
     if keyboard.is_pressed("Alt+x"):
         
-        takescreenshot(calc_xy(1500, 280, 420, 20))
-        text =  textdetection()
-        PipeId = searchcsv(text)
-        print(PipeId)
-        if PipeId == None:
+        #take Screenshot and resize image
+        takescreenshot(calc_xy(1500, 283, 420, 18))
+        resize_img("IDPhoto.png", 300, 24)
+
+        #detect text
+        text =  textdetection(IMG_PATH)
+
+        #search in CSV
+        PipeId = searchcsv(text, "IDs.csv")
+        print("PipeId= " + str(PipeId))
+        
+
+        if PipeId == -1:
+            #debug
             if debug:
                 print("PipeID not found")
         else:
+            #debug
             if debug:
-                
                 print("PipeID copied to clipboard")
-            toclipboard(PipeId)
-        
 
+            #copy to clipboard
+            toclipboard(str(PipeId))
+        
 if debug:
     print("Program ended")
 
