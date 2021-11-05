aa61e3be3a83f941daa1179a35191d825ccb073e
Lasse <doodle.11243@gmail.com>
2021-11-05 17:52:28 +0100

    mayor changes

--- main.py
+++ main.py
@@ -3,23 +3,28 @@ from PIL import Image
 import easyocr as ocr
 import cv2
 import pyautogui as pg
-import numpy as np
 import keyboard
 import os
+
 
 FILEPATH = os.getcwd()
 IMG_NAME = "IDPhoto.png"
 IMG_PATH = os.path.join(FILEPATH, "img", IMG_NAME)
+debug = True
 
 
 
-mcIDpos = [1500, 280, 420, 20]
+mcIDpos = [1500, 283, 420, 17]
 x1 = mcIDpos[0]
 y1 = mcIDpos[1]
 x2 = mcIDpos[0] + mcIDpos[2]
 y2 = mcIDpos[1] + mcIDpos[3]
 
 os.chdir("img")
+
+def toclipboard(text):
+    command = 'echo | set /p nul=' + text.strip() + '| clip'
+    os.system(command)
 
 def calc_xy(x, y, w, h):
     mcIDpos = [x, y, w, h]
@@ -40,53 +45,65 @@ def textdetection(Filepath=IMG_PATH):
         #Strip the Output for further processing
         Output = res[0][1]
         Output = Output[9:]
+        if debug:
+            print(Output)
 
         return Output
 
 def takescreenshot(Position=[x1, y1, x2, y2]):
         # Take Screenshot
         pg.screenshot(IMG_NAME)
-        print("Screenshot taken")
-        pg.sleep(1)
+        if debug:
+            print("Screenshot taken")
+        pg.sleep(0.1)
         
         #crop image to perfect size
         im = Image.open(IMG_PATH)
         im = im.crop((x1, y1, x2, y2))
+        if debug:
+            print("Image cropped")
         im.save(IMG_NAME)
+        if debug:
+            print("Image saved")
 
 def searchcsv(search_term):
     with open("IDs.csv", 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
+
+        if debug:
+            print("Search in CSV")
+
         for row in csv_reader:
             if search_term in row:
-                print(row[3])
-    return row[3] 
+                if debug:
+                    print("Found in CSV")
+                    print(row)
+                return row[3]
+                    
+            
+if debug:
+    print("Program started")
 
-while True:
-
+while keyboard.is_pressed('Alt+y') == False:
+    pg.sleep(0.05)
     #take Screenshot
     if keyboard.is_pressed("Alt+x"):
         
         takescreenshot(calc_xy(1500, 280, 420, 20))
+        text =  textdetection()
+        PipeId = searchcsv(text)
 
-        print(searchcsv(textdetection()))
-
-
-
-
+        if PipeId == "":
+            if debug:
+                print("PipeID not found")
+        else:
+            if debug:
+                
+                print("PipeID copied to clipboard")
+            toclipboard(PipeId)
         
 
+if debug:
+    print("Program ended")
 
-
-
-
-
-
-
-
-
-
-
-
-
-    
+print(PipeId)
